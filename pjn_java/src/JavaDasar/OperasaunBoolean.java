6932238660adcc0be2be49bd629563494eb5bcc6
ajito.costa <ajito.costa@viettel.com.vn>
2023-06-12 17:27:48 +0700

    Operasaun Boolean

---
+++ pjn_java/src/JavaDasar/OperasaunBoolean.java
@@ -0,0 +1,22 @@
+package JavaDasar;
+
+public class OperasaunBoolean {
+    public static void main(String[] args) {
+        //operasaun ne'ebe uza ba data boolean rua
+        int a = 10;
+        int b = 12;
+        int c = 10;
+        //&&        // Operator
+        //no                   //Keterangan
+        //no ne'e bele moss hanesan vezes hotu-hotu tenke loss mak nia foin fo true
+        System.out.println(a==c && a<=b);
+        //||        // Operator
+        //ka                  //Keterangan
+        //ka nee mais
+        System.out.println(a>=c || a==c);
+        //!         // Operator
+        //Kebalikan             //Keterangan
+        // negasaun
+        System.out.println(a!=b);
+    }
+}
