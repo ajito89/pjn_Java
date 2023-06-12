67e049c44fd47c77ef3a0825a37aaa675c61f0d4
ajito.costa <ajito.costa@viettel.com.vn>
2023-06-12 13:47:49 +0700

    Tipe Data Character

---
+++ pjn_java/src/JavaDasar/TipeDataCharacter.java
@@ -0,0 +1,12 @@
+package JavaDasar;
+
+public class TipeDataCharacter {
+    public static void main(String[] args) {
+        //char
+        //no ita uza petik satu iha awal no ikus ' ';
+        char a = 'a';
+        //hnasane sei error
+        //char b = 'ajiro';
+        System.out.println(a);
+    }
+}
