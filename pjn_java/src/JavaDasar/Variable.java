ef47850849e5bdbad6b3c391701b9d0b797aa48c
ajito.costa <ajito.costa@viettel.com.vn>
2023-06-12 14:12:05 +0700

    Variable

---
+++ pjn_java/src/JavaDasar/Variable.java
@@ -0,0 +1,33 @@
+package JavaDasar;
+
+public class Variable {
+    public static void main(String[] args) {
+        //mak fatin ida ne'ebe hodi rai data
+        String naran;
+        //naran ne'e hatudu varibale hodi rai data string ne'e
+        naran ="Ajito";
+        //ka
+        int age = 25;
+        String enderessu="Timor-Leste";
+
+        System.out.println(naran);
+        System.out.println(age);
+        System.out.println(enderessu);
+
+        //iha versaun 10 ba leten bele uza varibale ho deit halerek var
+        // maibe nia valor mak tenke tau hotu kedas antes
+        //labele husik mamuk
+        //exemplu sei error
+        //var ajito;//sei error
+        //ajito = "Ajito";//sei error
+        //la error
+        var tinan = 25;
+        var hela_fatin="Farol";
+
+        //hakrak halo varibale ne'ebe labele iha mudansa ita bele uza variable naran final
+        //dalabarak ema dehan kontant
+        final String nama= "Ajito Nelson";
+        System.out.println(nama);
+
+    }
+}
