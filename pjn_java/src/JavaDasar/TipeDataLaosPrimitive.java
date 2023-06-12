da0073f4d561d15c1ba82fa4df2bbea8fd9099ef
ajito.costa <ajito.costa@viettel.com.vn>
2023-06-12 15:16:34 +0700

    slide & Tipe Data laos Primitive

---
+++ pjn_java/src/JavaDasar/TipeDataLaosPrimitive.java
@@ -0,0 +1,45 @@
+package JavaDasar;
+
+public class TipeDataLaosPrimitive {
+    public static void main(String[] args) {
+        //tipe data primitve mak labele ubah lagi
+        //tipe data primitive selalu iha default value
+        //string laos primitive maibe nia object tamba nia defaul valu null
+        //Ezample Type data primitive
+        // byte
+        // short
+        // long
+        // int
+        // float
+        // double
+        //Eexemplu Type data laos primitive
+        // Byte
+        // Short
+        // Long
+        // Int
+        // Float
+        // Double
+
+        //EZEMPLU
+        String ajito ="Ajito";
+        Byte tinan=25;
+        Integer kha;
+        kha = null;
+        System.out.println(kha);
+
+        //konversi husi tipe data primitive ba non primitive
+        int tinann = 25;
+        Integer tinannObject=tinann;
+
+        int tinannAgain=tinannObject;
+
+        //maibe atu muda ba short ka byte ou seluk halo hansane
+        short shortAge=tinannObject.shortValue();
+        byte byteTinan=tinannObject.byteValue();
+
+
+
+
+
+    }
+}
