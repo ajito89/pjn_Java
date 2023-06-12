6ef20931e49171fd4a10855374f3b9baba63cf3c
ajito.costa <ajito.costa@viettel.com.vn>
2023-06-12 14:00:09 +0700

    Tipe Data String

---
+++ pjn_java/src/JavaDasar/TipeDataString.java
@@ -0,0 +1,13 @@
+package JavaDasar;
+
+public class TipeDataString {
+    public static void main(String[] args) {
+        //kumpilan karakter ka ni laran nee testu
+        //uza petik dua
+        //ni valor default null
+        String ajito = "Ajito";
+        String tinan= "18";
+
+
+    }
+}
