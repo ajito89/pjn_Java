d2305437bb0bcce27a2a6e44c1957050b8a91f60
ajito.costa <ajito.costa@viettel.com.vn>
2023-06-12 13:53:44 +0700

    Tpe data Bollean

---
+++ pjn_java/src/JavaDasar/TipeDataBolean.java
@@ -0,0 +1,15 @@
+package JavaDasar;
+
+public class TipeDataBolean {
+    public static void main(String[] args) {
+        //Tipe data buleean iha deit valor rua mak iha laran exeplu loss ou sala, true or false, 1 ou 0
+        //no valor sira ne'e sei representasi ho true no false
+        //valor default ba bollean mak false
+
+        boolean loss = true;
+        boolean sala = false;
+        System.out.println(loss);
+
+
+    }
+}
