d8d960145824ba8028cd5896e3af315eb7206502
ajito.costa <ajito.costa@viettel.com.vn>
2023-06-12 17:19:30 +0700

    Operasaun Komparasaun

---
+++ pjn_java/src/JavaDasar/OperasaunComparasaun.java
@@ -0,0 +1,30 @@
+package JavaDasar;
+
+public class OperasaunComparasaun {
+    public static void main(String[] args) {
+        int a =10;
+        int b = 20;
+        int c = 10;
+        //operasaun ida ne'e atu halo komparasaun data
+        //no nia sempre fo sai valor bollean
+        //>                         // Operator
+        //Lebih Dari                            //Keterangan
+        boolean ab = a > b;
+        System.out.println(ab);
+        //<                         // Operator
+        //Kurang Dari                           //Keterangan
+        //>=                        // Operator
+        //Lebih Dari Sama Dengan                //Keterangan
+        System.out.println(a >= b);
+        //<=                        // Operator
+        //Kurang Dari Sama Dengan               //Keterangan
+        System.out.println(a<=b);
+        //==                        // Operator
+        //Sama Dengan                           //Keterangan
+        System.out.println(a==c);
+        //!=                        // Operator
+        //Tidak Sama Dengan                     //Keterangan
+        boolean ac= a!=c;
+        System.out.println(ac);
+    }
+}
