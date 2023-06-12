85e7120c93e3130a646fb655f769b61fd6cb3810
ajito.costa <ajito.costa@viettel.com.vn>
2023-06-12 17:07:45 +0700

    Operasaun Matematika

---
+++ pjn_java/src/JavaDasar/OperasaunMatematika.java
@@ -0,0 +1,60 @@
+package JavaDasar;
+
+public class OperasaunMatematika {
+    public static void main(String[] args) {
+        //+ Penjumlahan
+        //- Pengurangan
+        //* Perkalian
+        // / Pembagian
+        //% Sisa Pembagian ka mod
+
+        //operasaun matematika
+        int a =10;
+        int b =11;
+        System.out.println(a + b);
+        System.out.println(b % a);
+
+        //Augmented Assignments
+        //a = a + 10    // Operasi Matematika
+        //a += 10       //Augmented Assignments
+        System.out.println(a += 10);
+        //a = a - 10    // Operasi Matematika
+        int aa= a -= 10;       //Augmented Assignments
+        System.out.println("a -= 10 "+ aa);
+        //a = a * 10    // Operasi Matematika
+        //a *= 10       //Augmented Assignments
+        System.out.println(a *= 10);
+        //a = a / 10    //Operasi Matematika
+        //a /= 10       //Augmented Assignments
+        System.out.println(a /= 10);
+        //a = a % 10    // Operasi Matematika
+        //a %= 10       //Augmented Assignments
+        System.out.println(a %= 10);
+
+        int z = 10;
+        //unary operator
+        //++                    // Operator
+        z++;
+        System.out.println(z);
+        ++z;
+        System.out.println(z);
+        //ab = ab + 1           //Keterangan
+        //--                    // Operator
+        z--;
+        System.out.println(z);
+        --z;
+        System.out.println(z);
+        //ab = ab - 1           //Keterangan
+        //-                     // Operator
+
+        //Negative              //Keterangan
+        //+                     // Operator
+        //Positive              //Keterangan
+        //!                     // Operator
+        //Boolean kebalikan     //Keterangan
+
+
+
+
+    }
+}
