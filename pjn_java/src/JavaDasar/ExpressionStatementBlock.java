47840a1ca27190a3744994d2e1beda479edee3cb
ajito.costa <ajito.costa@viettel.com.vn>
2023-06-13 09:16:02 +0700

    Expression Statement and Block

---
+++ pjn_java/src/JavaDasar/ExpressionStatementBlock.java
@@ -0,0 +1,30 @@
+package JavaDasar;
+
+import java.util.Date;
+
+public class ExpressionStatementBlock {
+    public static void main(String[] args) {
+        //kode hotu ne'ebe sei sai hanesan single value ne'e ita hanaran expreaasion
+        //no nia sai hanesan komponente utama husi statment
+        int valor;  // ida ne'e expression
+        valor = 10; //ida ne'e expression
+        System.out.println(valor = 100); // ida nee statment maibe nia laran valor = 100 ne'e expression
+
+        //statment mak kalimat kompletu husi bahasa
+        System.out.println(valor = 200); // ida nee statment maibe nia laran valor = 100 ne'e expression
+        //buat rua ne'e tau hamutuk ona ne'e ita bolu statement
+        int aVallue=100;
+        //assignment statement
+        double value = 102.980;
+        //increment statement
+        aVallue++;
+        //method invocation statement
+        System.out.println("Helllo World");
+        //object creation statement
+        Date date = new Date();
+
+
+        //Block ne'e kompostu husi statement ne'ebe husi 0 ou statment barak
+        //sempre hahu ho remata ho kurun kurawal {}
+    }//ida ne'e block
+}
